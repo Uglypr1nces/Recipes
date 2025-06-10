0e5718433a9477302ced2fb5598be7870d13532c
Shpat Avdiu <uglyprincess@Shpats-MacBook-Pro.local>
2025-06-10 11:31:07 +0200

    renaming

--- Sasquatch/asgi.py
+++ Sasquatch/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Recipes project.
+ASGI config for Sasquatch project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Recipes.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sasquatch.settings')
 
 application = get_asgi_application()
--- Sasquatch/wsgi.py
+++ Sasquatch/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Recipes project.
+WSGI config for Sasquatch project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Recipes.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sasquatch.settings')
 
 application = get_wsgi_application()
